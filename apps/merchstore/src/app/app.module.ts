12ac4476d2d737dd30b70c23dd19cd81b12ad1ed
Maina Wycliffe <wmmaina7@gmail.com>
2020-02-25 23:22:20 +0300

    Restructure the apps components

--- apps/merchstore/src/app/app.module.ts
+++ apps/merchstore/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
